3f77c4594fd0755567f4a586c84917c9911a128b
yue.lab <gitlab@rainy.me>
2018-12-02 01:06:36 +0900

    skip root and hidden files

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -33,26 +33,50 @@ fun String.suffix(suffix: String): String {
     return "$this.$suffix"
 }
 
+data class Config(
+    val input: String,
+    val output: String,
+    val overwrite: Boolean = false
+)
+
 
 fun main() = runBlocking {
 
     val exclude = mutableListOf<String>()
-    val dist = "test_dist/"
+    val config = Config(
+        input = "test_files",
+        output = "test_dist/"
+    )
 
-    File("test_files/")
-        .walk()
+    File(config.input)
+        .walkTopDown()
         .forEach {
-            launch {
-                it.copyTo(
-                    File(
-                        it
-                            .nameWithoutExtension
-                            .hash(exclude)
-                            .prefix(dist)
-                            .suffix(it.extension)
-                    ), overwrite = true
-                )
+            when {
+                it.name == config.input -> {
+                    println("skip root")
+                    return@forEach
+                }
+                it.name + '/' == config.input -> {
+                    println("skip root")
+                    return@forEach
+                }
+                it.name.startsWith('.') -> {
+                    println("skip hidden")
+                    return@forEach
+                }
+                else -> launch {
+                    it.copyTo(
+                        File(
+                            it
+                                .nameWithoutExtension
+                                .hash(exclude)
+                                .prefix(config.output)
+                                .suffix(it.extension)
+                        ), overwrite = config.overwrite
+                    )
 
+                }
             }
+
         }
 }
